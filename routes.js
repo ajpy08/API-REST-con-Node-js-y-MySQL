2f99c009072f9ee2b61cfc37c9f725d668715589
I.S.C. Angel Javier Puc Yamá <angelpuc08@gmail.com>
2021-01-21 15:30:24 -0600

    Final

--- routes.js
+++ routes.js
@@ -1,46 +1,42 @@
 const express = require('express')
 const routes = express.Router()
 
-routes.get('/', (req, res)=>{
-    req.getConnection((err, conn)=>{
-        if(err) return res.send(err)
+routes.get('/', (req, res) => {
+    req.getConnection((err, conn) => {
+        if (err) return res.send(err)
 
-        conn.query('SELECT * FROM books', (err, rows)=>{
-            if(err) return res.send(err)
-
-            res.json(rows)
+        conn.query('SELECT * FROM books', (err, books) => {
+            if (err) return res.send(err)
+            res.json(books)
         })
     })
 })
 
-routes.post('/', (req, res)=>{
-    req.getConnection((err, conn)=>{
-        if(err) return res.send(err)
-        conn.query('INSERT INTO books set ?', [req.body], (err, rows)=>{
-            if(err) return res.send(err)
-
+routes.post('/', (req, res) => {
+    req.getConnection((err, conn) => {
+        if (err) return res.send(err)
+        conn.query('INSERT INTO books set ?', [req.body], (err, rows) => {
+            if (err) return res.send(err)
             res.send('book added!')
         })
     })
 })
 
-routes.delete('/:id', (req, res)=>{
-    req.getConnection((err, conn)=>{
-        if(err) return res.send(err)
-        conn.query('DELETE FROM books WHERE id = ?', [req.params.id], (err, rows)=>{
-            if(err) return res.send(err)
-
+routes.delete('/:id', (req, res) => {
+    req.getConnection((err, conn) => {
+        if (err) return res.send(err)
+        conn.query('DELETE FROM books WHERE id = ?', [req.params.id], (err, rows) => {
+            if (err) return res.send(err)
             res.send('book excluded!')
         })
     })
 })
 
-routes.put('/:id', (req, res)=>{
-    req.getConnection((err, conn)=>{
-        if(err) return res.send(err)
-        conn.query('UPDATE books set ? WHERE id = ?', [req.body, req.params.id], (err, rows)=>{
-            if(err) return res.send(err)
-
+routes.put('/:id', (req, res) => {
+    req.getConnection((err, conn) => {
+        if (err) return res.send(err)
+        conn.query('UPDATE books set ? WHERE id = ?', [req.body, req.params.id], (err, rows) => {
+            if (err) return res.send(err)
             res.send('book updated!')
         })
     })
